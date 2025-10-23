08c5658d8d50e4f87d978a627c93f23f27b70e6a
l3130 <kmett32@gmail.com>
2025-10-23 13:19:59 +0100

    Initial commit

---
+++ Books.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+    const newBook = new Book(title, author, pages, isRead);
+    myLibrary.push(newBook);
+    return newBook;
+}
